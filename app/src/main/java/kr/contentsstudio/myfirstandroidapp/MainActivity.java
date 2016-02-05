a7e74c32c7cf8ecc746eb99e1b0f1257d1849707
hoyoung lee <hyleekr9580@gmail.com>
2016-02-05 13:58:13 +0900

    Activity

--- app/src/main/java/kr/contentsstudio/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/kr/contentsstudio/myfirstandroidapp/MainActivity.java
@@ -1,18 +1,21 @@
 package kr.contentsstudio.myfirstandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private TextView mTextView;
+    private EditText mNameEditText;
+    private EditText mAgeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         //화면에 레이아웃 표시
         setContentView(R.layout.checkbox);
 
+        // 이름, 나이를 xml에서 거져 와야 한다.
+        mNameEditText = (EditText) findViewById(R.id.name_edit_text);
+        mAgeEditText = (EditText) findViewById(R.id.age_edit_text);
+
+        // 버튼을 보여줄꺼다~~~
+        findViewById(R.id.next_activity_botton).setOnClickListener(this);
+
+        // 체크박스 글씨 가져온다~
         mTextView = (TextView) findViewById(R.id.msg_text_view);
         ((CheckBox) findViewById(R.id.check_box)).setOnCheckedChangeListener(this);
     }
@@ -36,6 +47,26 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         } else {
             mTextView.setVisibility(View.INVISIBLE);
         }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // 이름, 나이 값을 가져오기
+        // 테스트 Toast.makeText(MainActivity.this, "CHECK :" + mNameEditText.getText(), Toast.LENGTH_SHORT).show();
+        mNameEditText.getText();
+        mAgeEditText.getText();
+
+
+        //세컨드 엑티비티로 전환하겠다는 intent
+        Intent intent = new Intent(this, SecoundActivity.class);
+
+        // 이름 나이를 가져와서 intent에 추가
+        intent.putExtra("name", mNameEditText.getText().toString());
+        intent.putExtra("age", mAgeEditText.getText().toString());
+
+        //intent의 정보를 토대로 다른 엑티비티 실행
+        startActivity(intent);
     }
 }
 
---
+++ app/src/main/java/kr/contentsstudio/myfirstandroidapp/SecoundActivity.java
@@ -0,0 +1,54 @@
+package kr.contentsstudio.myfirstandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by 호영 on 2016-02-05.
+ */
+public class SecoundActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mNameTextView;
+    private TextView mAgeTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.checkbox_secound);
+
+        mNameTextView = (TextView) findViewById(R.id.name_text_view);
+        mAgeTextView = (TextView) findViewById(R.id.age_text_view);
+        findViewById(R.id.exit_button).setOnClickListener(this);
+
+
+        //나를 호출한 Intent 를 얻는다. 어떤놈이 호출한겨
+        Intent intent = getIntent();
+        // if 사용하는 이유 / 누구한테 값을 받아서 뿌려지는데... 해당 클래스만 단독으로 띄워야 하는
+        // 이유가 있다. 값이 없을 때 null 오류가 발생 한다.
+        //습관적으로  null 체크를 해야 한다.
+        if (intent != null) {
+            //이름과 나이를 얻어서 셋팅을 한다.
+            String name = intent.getStringExtra("name");
+            String age = intent.getStringExtra("age");
+
+            mNameTextView.setText(mNameTextView.getText().toString() + name);
+            mAgeTextView.setText(mAgeTextView.getText().toString() + age);
+
+
+        }
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // 현재 엑티비티 종료 한다. backkey 누른것과 동일하다.
+        Toast.makeText(SecoundActivity.this, "종료합니다.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
