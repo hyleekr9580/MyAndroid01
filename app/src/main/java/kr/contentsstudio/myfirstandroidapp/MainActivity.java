b97dc2c336a1ee70a9ee4eb264a8d1d5d45ee349
hoyoung lee <hyleekr9580@gmail.com>
2016-02-05 10:23:36 +0900

    체크박스

--- app/src/main/java/kr/contentsstudio/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/kr/contentsstudio/myfirstandroidapp/MainActivity.java
@@ -2,48 +2,39 @@ package kr.contentsstudio.myfirstandroidapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 추가 사용 하는... 화면에 레이아웃을 표시한다.
-        // R(리소스) : 안드로이드(현재프로젝트, 클래스파일을 제외하고...) 자원을 사용한다.
-        setContentView(R.layout.button);
+        //화면에 레이아웃 표시
+        setContentView(R.layout.checkbox);
 
-        //인터페이스의 메소드 생성 할 수 있다.
-        //익명클래스이다.
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "클릭 매우 잘됨", Toast.LENGTH_SHORT).show();
-
-            }
-        };
-        Button button2 = (Button) findViewById(R.id.button2);
-        //setOnClickListener 클릭이벤트를 처리 할 수 있는 Listener를 추가
-        // 클릭할 수 있는 객체를 던져 준다.
-        button2.setOnClickListener(this);
+        mTextView = (TextView) findViewById(R.id.msg_text_view);
+        ((CheckBox) findViewById(R.id.check_box)).setOnCheckedChangeListener(this);
     }
+
 
     @Override
-    public void onClick(View v) {
-        Toast.makeText(MainActivity.this, "클릭 매우 잘됨", Toast.LENGTH_SHORT).show();
-    }
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Toast.makeText(MainActivity.this, "CHECK" + isChecked, Toast.LENGTH_SHORT).show();
 
-//    // botton.xml 에 연결됨
-//    public void onClick(View view) {
-//        Log.d(TAG, "클릭 잘됨");
-//        Toast.makeText(MainActivity.this, "클릭 잘됨", Toast.LENGTH_SHORT).show();
-//
-//
-//    }
+        //텍스트 뷰의 글자를 변경 할거다.
+        if (isChecked) {
+            mTextView.setText("체크됨");
+        } else {
+            mTextView.setText("");
+        }
+    }
 }
+
